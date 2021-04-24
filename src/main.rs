f7f38d6fc50b1ca782e6a2d04d43495596cdedc3
loxygenk <me@loxygen.dev>
2021-04-24 21:53:18 +0900

    ✨ Parse yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod config;
+
+use std::io::prelude::*;
+use std::fs::File;
+use config::InstallConfig;
+
 fn main() {
-    println!("Hello, world!");
+    let target = std::env::args()
+        .skip(1)
+        .next()
+        .expect("File name to parse");
+
+    let mut config_file = String::new();
+    File::open(&target)
+        .expect("Error occurred when opening file: not existing file?")
+        .read_to_string(&mut config_file)
+        .expect("Error occurred when reading the file");
+
+    let config: InstallConfig = serde_yaml::from_str(&&config_file)
+        .expect("Unable to parse yaml file");
+    
+    println!("{:?}", config);
 }
